2cca22520fba4ef2fd10ca58b2c2b6e1d9a6eca7
David Lopez <dlopez@mattmen.com>
2022-02-23 10:37:32 -0500

    Imported about-section.blade from old project.

--- resources/views/livewire/about-section.blade.php
+++ resources/views/livewire/about-section.blade.php
@@ -1,69 +1,81 @@
-<div class="relative bg-white py-16 sm:py-24" id="about">
+<section id="about" class="relative bg-white py-16 sm:py-24">
     <div class="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:gap-24 lg:items-start">
-      <div class="relative sm:py-16 lg:py-0">
-        <div aria-hidden="true" class="hidden sm:block lg:absolute lg:inset-y-0 lg:right-0 lg:w-screen">
-          <div class="absolute inset-y-0 right-1/2 w-full bg-gray-50 rounded-r-3xl lg:right-72"></div>
-          <svg class="absolute top-8 left-1/2 -ml-3 lg:-right-8 lg:left-auto lg:top-12" width="404" height="392" fill="none" viewBox="0 0 404 392">
-            <defs>
-              <pattern id="02f20b47-fd69-4224-a62a-4c9de5c763f7" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
-                <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
-              </pattern>
-            </defs>
-            <rect width="404" height="392" fill="url(#02f20b47-fd69-4224-a62a-4c9de5c763f7)" />
-          </svg>
-        </div>
-        <div class="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0 lg:max-w-none lg:py-20">
-          <!-- Testimonial card-->
-          <div class="relative pt-64 pb-10 rounded-2xl shadow-xl overflow-hidden">
-            <img class="absolute inset-0 h-full w-full object-cover" src="{{ asset('img/50_Central_Park_West,_NYC_(12).jpg') }}" alt="Chimney at 50 Central Park West">
-            <div class="absolute inset-0 bg-indigo-500 mix-blend-multiply"></div>
-            <div class="absolute inset-0 bg-gradient-to-t from-indigo-200 via-indigo-600 opacity-70"></div>
-            <div class="relative px-8">
-              <div>
-                <img class="h-12 hidden" src="https://tailwindui.com/img/logos/workcation.svg?color=white" alt="Workcation">
-              </div>
-              <blockquote class="mt-30">
-                <div class="relative text-lg font-medium text-white md:flex-grow">
-                 
-                  <p class="relative">
-                    "It brings me great joy to know that we can provide the assistance a customer needs to pass inspection, maintain their existing environment and meet their project deadlines with amazing work!"
-                  </p>
-                </div>
-  
-                <footer class="mt-4">
-                  <p class="text-base font-semibold text-indigo-200">Robert Paolillo, CEO at Lusheba Designs</p>
-                </footer>
-              </blockquote>
+        <div class="relative sm:py-16 lg:py-0">
+            <div aria-hidden="true" class="hidden sm:block lg:absolute lg:inset-y-0 lg:right-0 lg:w-screen">
+                <div class="absolute inset-y-0 right-1/2 w-full bg-gray-50 rounded-r-3xl lg:right-72"></div>
+                <svg class="absolute top-8 left-1/2 -ml-3 lg:-right-8 lg:left-auto lg:top-12" width="404" height="392"
+                    fill="none" viewBox="0 0 404 392">
+                    <defs>
+                        <pattern id="02f20b47-fd69-4224-a62a-4c9de5c763f7" x="0" y="0" width="20" height="20"
+                            patternUnits="userSpaceOnUse">
+                            <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+                        </pattern>
+                    </defs>
+                    <rect width="404" height="392" fill="url(#02f20b47-fd69-4224-a62a-4c9de5c763f7)" />
+                </svg>
             </div>
-          </div>
+            <div class="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0 lg:max-w-none lg:py-20">
+                <!-- Testimonial card-->
+                <div class="relative pt-64 pb-10 rounded-2xl shadow-xl overflow-hidden">
+                    <img class="absolute inset-0 h-full w-full object-cover"
+                        src="{{ asset('img/50_Central_Park_West,_NYC_(12).jpg') }}"
+                        alt="Chimney at 50 Central Park West">
+                    <div class="absolute inset-0 bg-indigo-500 mix-blend-multiply"></div>
+                    <div class="absolute inset-0 bg-gradient-to-t from-indigo-200 via-indigo-600 opacity-70"></div>
+                    <div class="relative px-8">
+                        <div>
+                            <img class="h-12 hidden"
+                                src="https://tailwindui.com/img/logos/workcation.svg?color=white" alt="Workcation">
+                        </div>
+                        <blockquote class="mt-30">
+                            <div class="relative text-lg font-medium text-white md:flex-grow">
+
+                                <p class="relative">
+                                    "It brings me great joy to know that we can provide the assistance a customer needs
+                                    to pass inspection, maintain their existing environment and meet their project
+                                    deadlines with amazing work!"
+                                </p>
+                            </div>
+
+                            <footer class="mt-4">
+                                <p class="text-base font-semibold text-indigo-200">Robert Paolillo, CEO at Lusheba
+                                    Designs</p>
+                            </footer>
+                        </blockquote>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-  
-      <div class="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0">
-        <!-- Content area -->
-        <div class="pt-12 sm:pt-16 lg:pt-20">
-          <h2 class="text-3xl text-gray-900 font-extrabold tracking-tight sm:text-4xl">
-            About Us
-          </h2>
-          <div class="mt-6 text-gray-500 space-y-6">
-            
-            <h3 class="font-extrabold text-gray-900">Our Mission is to empower our partners</h3>
-            <p class="text-base leading-7 text-justify">
-              We want to provide our customers/partners the information they need to protect their investments and continue to execute their business goals.  As a company built from the ground up, we understand what it takes to provide the assistance needed to help you grow in the same fashion.
-            </p>
-  
-            <h3 class="font-extrabold text-gray-900">Company Vision, Mission and Values</h3>
-            <p class="text-base leading-7">
-              We take pride in rendering engineers, architects, and contractors helpful information to promote wonderful work experiences for building owners, building management companies, engineers, architects, and mechanical contractors.
-            </p>
-  
-            {{-- <h3 class="font-extrabold text-gray-900">The Owner</h3>
+
+        <div class="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0">
+            <!-- Content area -->
+            <div class="pt-12 sm:pt-16 lg:pt-20">
+                <h2 class="text-3xl text-gray-900 font-extrabold tracking-tight sm:text-4xl">
+                    About Us
+                </h2>
+                <div class="mt-6 text-gray-500 space-y-6">
+
+                    <h3 class="font-extrabold text-gray-900">Our Mission is to empower our partners</h3>
+                    <p class="text-base leading-7 text-justify">
+                        We want to provide our customers/partners the information they need to protect their investments
+                        and continue to execute their business goals. As a company built from the ground up, we
+                        understand what it takes to provide the assistance needed to help you grow in the same fashion.
+                    </p>
+
+                    <h3 class="font-extrabold text-gray-900">Company Vision, Mission and Values</h3>
+                    <p class="text-base leading-7">
+                        We take pride in rendering engineers, architects, and contractors helpful information to promote
+                        wonderful work experiences for building owners, building management companies, engineers,
+                        architects, and mechanical contractors.
+                    </p>
+
+                    {{-- <h3 class="font-extrabold text-gray-900">The Owner</h3>
             <p class="text-base leading-7">
               Robert Paolillo, the owner, is a college graduate in aerospace engineering. He has always enjoyed working with his hands since he was a young boy.  He welcomes the challenges each job brings and has an enormous drive to get your issues resolved or prevent a problem from arising.
             </p> --}}
-          </div>
+                </div>
+            </div>
+
         </div>
-  
-      </div>
     </div>
-  </div>
+</section>
