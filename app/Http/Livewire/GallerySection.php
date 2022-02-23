8133798ad83b09fdc905ed6c03cac40911f6dd77
David Lopez <dlopez@mattmen.com>
2022-02-23 08:38:00 -0500

    Initial Commit

---
+++ app/Http/Livewire/GallerySection.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class GallerySection extends Component
+{
+    public function render()
+    {
+        return view('livewire.gallery-section');
+    }
+}
