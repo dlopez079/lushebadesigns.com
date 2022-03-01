f709ff6b976b1759e553bbc000f8eefa2eefa4ce
David Lopez <dlopez@mattmen.com>
2022-03-01 13:18:25 -0500

    Contact Section Modification. I moved the contact information around so that it aligns nicely in mobil view.

--- resources/views/livewire/contact-section.blade.php
+++ resources/views/livewire/contact-section.blade.php
@@ -8,21 +8,7 @@
 
                 <div class="mt-8 grid grid-cols-1 gap-12 sm:grid-cols-2 sm:gap-x-8 sm:gap-y-12 lg:mt-0 lg:col-span-2">
                     <div>
-                        <h3 class="text-lg leading-6 font-medium text-white">Contact Info</h3>
-                        <dl class="mt-2 text-base text-white">
-                            <div>
-                                <dt class="sr-only">Email</dt>
-                                <dd>info@lushebadesigns.com</dd>
-                            </div>
-                            <div class="mt-1">
-                                <dt class="sr-only">Phone</dt>
-                                <dd>+1 (718) 348-0636</dd>
-                            </div>
-                        </dl>
-                    </div>
-                    <div>
-                        <h3 class="text-lg leading-6 font-medium text-white">Nikolai Designs Systems dba Lusheba Designs
-                        </h3>
+                        <h3 class="text-lg leading-6 font-medium text-white">Nikolai Designs Systems dba Lusheba Designs</h3>
                         <dl class="mt-2 text-base text-white">
                             <div>
                                 <dt class="sr-only">Address</dt>
@@ -31,6 +17,18 @@
                             <div class="mt-1">
                                 <dt class="sr-only">City, State, Zip</dt>
                                 <dd>Bronx, NY, 10457</dd>
+                            </div>
+                        </dl>
+                    </div>
+                    <div>
+                        <dl class="mt-2 text-base text-white">
+                            <div>
+                                <dt class="sr-only">Email</dt>
+                                <dd>info@lushebadesigns.com</dd>
+                            </div>
+                            <div class="mt-1">
+                                <dt class="sr-only">Phone</dt>
+                                <dd>+1 (718) 348-0636</dd>
                             </div>
                         </dl>
                     </div>
