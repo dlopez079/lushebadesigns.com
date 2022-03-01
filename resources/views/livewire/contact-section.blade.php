0faab5e1440c65b34e2691e83ec20b4f146454df
David Lopez <dlopez@mattmen.com>
2022-03-01 13:01:04 -0500

    Modified Size

--- resources/views/livewire/contact-section.blade.php
+++ resources/views/livewire/contact-section.blade.php
@@ -8,15 +8,15 @@
 
                 <div class="mt-8 grid grid-cols-1 gap-12 sm:grid-cols-2 sm:gap-x-8 sm:gap-y-12 lg:mt-0 lg:col-span-2">
                     <div>
-                        <h3 class="text-lg leading-6 font-medium text-white">Collaborate</h3>
+                        <h3 class="text-lg leading-6 font-medium text-white">Contact Info</h3>
                         <dl class="mt-2 text-base text-white">
                             <div>
                                 <dt class="sr-only">Email</dt>
                                 <dd>info@lushebadesigns.com</dd>
                             </div>
                             <div class="mt-1">
-                                <dt class="sr-only">Website</dt>
-                                <dd>www.lushebadesigns.com</dd>
+                                <dt class="sr-only">Phone</dt>
+                                <dd>+1 (718) 348-0636</dd>
                             </div>
                         </dl>
                     </div>
@@ -30,7 +30,7 @@
                             </div>
                             <div class="mt-1">
                                 <dt class="sr-only">City, State, Zip</dt>
-                                <dd>+1 (718) 348-0636</dd>
+                                <dd>Bronx, NY, 10457</dd>
                             </div>
                         </dl>
                     </div>
