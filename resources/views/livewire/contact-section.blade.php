66d51ce475be99e61e7888723c9d09ce498c0f56
David Lopez <dlopez@mattmen.com>
2022-03-08 13:37:34 -0500

    Contact Section Modification. I modified the layout of the contact section of the landing site.

--- resources/views/livewire/contact-section.blade.php
+++ resources/views/livewire/contact-section.blade.php
@@ -3,36 +3,30 @@
         <div class="divide-y-2 divide-gray-200">
 
             {{-- GET In touch --}}
-            <div class="lg:grid lg:grid-cols-3 lg:gap-8">
-                <h2 class="text-2xl font-extrabold text-white sm:text-3xl">Get in touch</h2>
-
-                <div class="mt-8 grid grid-cols-1 gap-12 sm:grid-cols-2 sm:gap-x-8 sm:gap-y-12 lg:mt-0 lg:col-span-2">
-                    <div>
-                        <h3 class="text-lg leading-6 font-medium text-white">Nikolai Designs Systems dba Lusheba Designs</h3>
-                        <dl class="mt-2 text-base text-white">
-                            <div>
-                                <dt class="sr-only">Address</dt>
-                                <dd>4228-38 Park Avenue</dd>
-                            </div>
-                            <div class="mt-1">
-                                <dt class="sr-only">City, State, Zip</dt>
-                                <dd>Bronx, NY, 10457</dd>
-                            </div>
-                        </dl>
-                    </div>
-                    <div>
-                        <dl class="mt-2 text-base text-white">
-                            <div>
-                                <dt class="sr-only">Email</dt>
-                                <dd>info@lushebadesigns.com</dd>
-                            </div>
-                            <div class="mt-1">
-                                <dt class="sr-only">Phone</dt>
-                                <dd>+1 (718) 348-0636</dd>
-                            </div>
-                        </dl>
-                    </div>
-
+            <div class="lg:grid lg:grid-cols-2 lg:gap-8">
+                <div>
+                    <h2 class="text-2xl font-extrabold text-white sm:text-3xl">Get in Touch</h2>
+                </div>
+                <div>
+                    <h3 class="text-lg leading-6 font-medium text-white">Nikolai Designs Systems dba Lusheba Designs</h3>
+                    <dl class="mt-2 text-base text-white">
+                        <div>
+                            <dt class="sr-only">Address</dt>
+                            <dd>4228-38 Park Avenue</dd>
+                        </div>
+                        <div class="mt-1">
+                            <dt class="sr-only">City, State, Zip</dt>
+                            <dd>Bronx, NY, 10457</dd>
+                        </div>
+                        <div class="mt-1">
+                            <dt class="sr-only">Email</dt>
+                            <dd>info@lushebadesigns.com</dd>
+                        </div>
+                        <div class="mt-1">
+                            <dt class="sr-only">Phone</dt>
+                            <dd>+1 (718) 348-0636</dd>
+                        </div>
+                    </dl>
                 </div>
             </div>
 
